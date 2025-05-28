fcc17005eb25654108b12bedccc6c55266a3116c
Roberto-Kaiser <159036437+Roberto-Kaiser@users.noreply.github.com>
2025-05-28 09:42:04 -0300

    Create entrega4.py

---
+++ entrega4.py
@@ -0,0 +1,76 @@
+for i in range(1, 101):
+    print(i)
+    if i == 2:
+        print("É primo")
+    
+    if i == 3:
+        print("É primo") 
+        
+    if i == 5:
+        print("É primo")
+        
+    if i == 7:
+        print("É primo")    
+        
+    if i == 11:
+        print("É primo")    
+        
+    if i == 13:
+        print("É primo")    
+        
+    if i == 17:
+        print("É primo")   
+        
+    if i == 19:
+        print("É primo")
+        
+    if i == 23:
+        print("É primo")  
+        
+    if i == 29:
+        print("É primo") 
+        
+    if i == 31:
+        print("É primo")
+        
+    if i == 37:
+        print("É primo") 
+        
+    if i == 41:
+        print("É primo")  
+        
+    if i == 43:
+        print("É primo")  
+        
+    if i == 47:
+        print("É primo") 
+        
+    if i == 53:
+        print("É primo")    
+        
+    if i == 59:
+        print("É primo")   
+        
+    if i == 61:
+        print("É primo")    
+        
+    if i == 67:
+        print("É primo")
+        
+    if i == 71:
+        print("É primo")  
+        
+    if i == 73:
+        print("É primo")    
+        
+    if i == 79:
+        print("É primo")  
+        
+    if i == 83:
+        print("É primo")    
+        
+    if i == 89:
+        print("É primo")
+        
+    if i == 97:
+        print("É primo")    
